c03f7f9b672fab96b8b5656386e3c7ebcb896d88
balan-ur-buddy <bugbountyhunter1234@gmail.com>
2026-05-11 19:38:43 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/vegicious/app/vegicious/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.vegicious.app.vegicious
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360,690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // localizationsDelegates: const [
+        //   GlobalMaterialLocali
+        // ],
+        supportedLocales: [
+          Locale('en','US')
+        ],
+        title: 'Vegecious',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: Scaffold(body: Center(child: Text('Shop Groceries!!'),)),
+      ),
+    );
+  }
+}
