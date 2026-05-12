71fc39d79a6c28ed024eafe142d6076e41a05964
balan-ur-buddy <bugbountyhunter1234@gmail.com>
2026-05-12 11:23:50 +0530

    splash screen

--- lib/features/splash/splashscreen.dart
+++ lib/features/splash/splashscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget{
   const SplashScreen({super.key});
@@ -6,8 +8,30 @@ class SplashScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF2E99C7),
       body : Center(
-        child: Text('Splash'),
+        child: splashLoadingIcon(),
+      ),
+    );
+  }
+
+  Widget splashLoadingIcon(){
+    return SizedBox(
+     width: 110.w,
+     height: 110.h,
+      child: Container(
+         decoration: ShapeDecoration(
+             shape: RoundedRectangleBorder(
+               borderRadius: BorderRadiusGeometry.circular(20.0)
+             )
+         ),
+        child: Center(
+          child: LottieBuilder.asset(
+            'assets/animations/splashLoading.json',
+            width: 50.w,
+            height: 37.h,
+          ),
+        ),
       ),
     );
   }
