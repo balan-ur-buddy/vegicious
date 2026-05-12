dec8f9c8c48db7fcb6874d99b84ddd5551d7817b
balan-ur-buddy <bugbountyhunter1234@gmail.com>
2026-05-12 12:01:40 +0530

    splash screen

--- lib/features/splash/splashscreen.dart
+++ lib/features/splash/splashscreen.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
+import 'package:vegicious/config/pagerouter.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatefulWidget{
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() {
+    return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<SplashScreen>{
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(seconds : 3),(){
+      if(mounted){
+        Navigator.pushReplacementNamed(context, PageRouter.about);
+    }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
